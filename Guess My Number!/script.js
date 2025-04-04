3bb1f2adcf8c5daaabe4d4f0a997cd8930f928ef
Lungelwa Mazibuko <infolungelwamazibuko@gmail.com>
2025-04-04 06:34:59 +0000

    Comments on script.js

--- Guess My Number!/script.js
+++ Guess My Number!/script.js
@@ -1,13 +1,17 @@
 'use strict';
-// '.' is a class selector '#' is a id selector
+/*
+NOTE: '.' is a class selector '#' is a id selector
 console.log(document.querySelector('.message').textContent)
 document.querySelector('.message').textContent = '🎉SCorrect Number!';
 console.log(document.querySelector('.message').textContent)
 
-// Data Testing DOM Manipulation 
+Data Testing DOM Manipulation 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
+Data Testing Vaules
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value)
+*/
 
+// Event listener with button configuration
