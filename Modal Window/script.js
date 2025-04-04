606b84f461c24605fdd4a1a296bc6aafa287ac6d
Lungelwa Mazibuko <infolungelwamazibuko@gmail.com>
2025-04-04 14:28:53 +0000

    Created Variables in script.js

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.show-modal');
+console.log(btnOpenModal);
+
+// TODO: COMPLETE THE CODE
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
