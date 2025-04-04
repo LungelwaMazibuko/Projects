e0e1bdd821f8da590a6bf9c9c3513c59b2f73808
Lungelwa Mazibuko <infolungelwamazibuko@gmail.com>
2025-04-04 08:26:59 +0000

    🔴Fixed bug

--- Guess My Number!/script.js
+++ Guess My Number!/script.js
@@ -40,7 +40,7 @@ document.querySelector('.check').addEventListener('click', function(){
     if (!guess){ //Boolean Expresseion
         document.querySelector('.message').textContent = '⛔ No number!';
     }else if (guess === secretNumber) {
-        document.querySelector('.message.').textContent = '🎉 Correct Number!';
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
     }else if (guess > secretNumber){
         document.querySelector('.message').textContent = '📈 Too high';
     }else if (guess < secretNumber) {
