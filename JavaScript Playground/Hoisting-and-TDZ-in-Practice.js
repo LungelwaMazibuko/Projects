3defb4b81a2ca70a8f18cd314c1e052a2ef44b3a
Lungelwa Mazibuko <infolungelwamazibuko@gmail.com>
2025-04-09 05:55:19 +0000

    Add initial implementation of hoisting and TDZ examples

---
+++ JavaScript Playground/Hoisting-and-TDZ-in-Practice.js
@@ -0,0 +1,34 @@
+'use strict';
+// Hoisting and TDZ in Practice
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShopingCart();
+
+var numProducts = 10;
+
+function deleteShopingCart() {
+  console.log('All products deleted!');
+}
