6aef3744ec4f7c190ad8259503f31098ad050ee3
Lungelwa Mazibuko <infolungelwamazibuko@gmail.com>
2025-04-09 05:37:05 +0000

    Fix printAge function to correctly log age and birth year

--- JavaScript Playground/scoopingInPractice.js
+++ JavaScript Playground/scoopingInPractice.js
@@ -2,7 +2,10 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
+    function printAge() {
+        const output = `You are ${age}, born in ${birthYear}`;
+        console.log(outout);
+    }
   return age;
 }
 
