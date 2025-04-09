6d6ec760db9341192026d5d0f8dacc4a85bdd6b6
Lungelwa Mazibuko <infolungelwamazibuko@gmail.com>
2025-04-09 07:29:58 +0000

    Add jonas object with methods to calculate age and greet

---
+++ JavaScript Playground/Regular-Functions-vs-Arrow-Functions.js
@@ -0,0 +1,19 @@
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
