1f11dec4f3a7a152d575d1b21333c0ea4f02fd9a
Lungelwa Mazibuko <infolungelwamazibuko@gmail.com>
2025-04-09 10:35:58 +0000

    Add initial implementation of age calculation and printing functions

--- JavaScript Playground/script.js
+++ JavaScript Playground/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Scooping in Practice
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  return age;
+}
+function printAge() {
+  const output = `You are ${age}, born in ${birthYear}`;
+  console.log(output);
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+console.log(printAge);
