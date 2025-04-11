41288138065546ea52db26a8137fc32ed3a1869b
lungelwa-mazibuko <lungelwa.mazibuko.dev@gmail.com>
2025-04-11 08:28:33 +0200

    Format orderDelivery console log for improved readability

--- JavaScript Playground/Data Sctructures, Modern Operators and Strings/script.js
+++ JavaScript Playground/Data Sctructures, Modern Operators and Strings/script.js
@@ -33,9 +33,11 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
   orderDelivery: function (starterIndex, mainIndex, time, address) {
-    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
   },
-    openingHours: {
+  openingHours: {
     thu: {
       open: 12,
       close: 22,
@@ -56,8 +58,9 @@ restaurant.orderDelivery({
   address: 'Via del Sole, 21',
   mainIndex: 2,
   starterIndex: 2,
-}); 
+});
 
+// Destructuring assignment
 const { name, openingHours, categories } = restaurant; // Destructuring assignment
 console.log(name, openingHours, categories); // Classico Italiano {thu: {…}, fri: {…}, sat: {…}} (4) ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']
 
