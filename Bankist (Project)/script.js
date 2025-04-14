3699850b9727e4c8d57df43b6b44c54d03e161dc
lungelwa-mazibuko <lungelwa.mazibuko.dev@gmail.com>
2025-04-14 13:15:55 +0200

    Add filtering for deposits and withdrawals from movements

--- Bankist (Project)/script.js
+++ Bankist (Project)/script.js
@@ -209,3 +209,17 @@ createUsername(accounts);
 console.log(accounts);
 */
 
+const desposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements); // [200, 450, -400, 3000, -650, -130, 70, 1300]
+console.log(desposits); // [200, 450, 3000, 70, 1300]
+
+const depositsFor = [];
+for (const mov of movements) {
+  if (mov > 0) depositsFor.push(mov);
+}
+console.log(depositsFor); // [200, 450, 3000, 70, 1300]
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals); // [-400, -650, -130]
