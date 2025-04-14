ab7d8f3d5120ad763c1ff5abf9f93cd3c343136d
lungelwa-mazibuko <lungelwa.mazibuko.dev@gmail.com>
2025-04-14 12:03:08 +0200

    Add displayMovements function to render account movements and comment out opacity in nav styles

--- Bankist (Project)/script.js
+++ Bankist (Project)/script.js
@@ -152,3 +152,19 @@ currenciesUnique.forEach(function (value, _, map) {
   console.log(`${key}: ${value}`);
 });
 */
+
+const displayMovements = function (movements) {
+  movements.forEach(function(mov, i){
+const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  })
+};
+
+displayMovements(account1.movements);
