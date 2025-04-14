720598b8912f3d494825eecd5f97a05d1c9ed3fd
lungelwa-mazibuko <lungelwa.mazibuko.dev@gmail.com>
2025-04-14 14:06:46 +0200

    Add balance and summary calculations for account movements

--- Bankist (Project)/script.js
+++ Bankist (Project)/script.js
@@ -250,3 +250,27 @@ const totalDepositsUSD = movenents
   .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const income = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${income} EUR`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)} EUR`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .reduce((acc, int) => acc + int, 0);
+};
+calcDisplaySummary(account1.movements);
