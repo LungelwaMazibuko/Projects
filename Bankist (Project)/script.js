c3b6f66e476608bbbbfb316848d237d0dac623ec
lungelwa-mazibuko <lungelwa.mazibuko.dev@gmail.com>
2025-04-14 11:03:37 +0200

    Add examples of array indexing and slicing methods

--- Bankist (Project)/script.js
+++ Bankist (Project)/script.js
@@ -111,3 +111,11 @@ console.log(letters.join(' - ')); // 'a - b - c - d - e - j - i - h - g - f'
 
 */
 
+const arr = [23, 11, 64];
+console.log(arr[0]); // 23
+console.log(arr.at(0)); // 23
+
+// Getting last element
+console.log(arr[arr.length - 1]); // 64
+console.log(arr.slice(-1)[0]); // 64
+console.log(arr.at(-1)); // 64
